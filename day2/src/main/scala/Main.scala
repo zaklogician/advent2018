70d5114dde051f133d4cce5c374e3a5f86045dcf
Zoltan A. Kocsis <djfishlips@gmail.com>
2018-12-07 05:32:56 +0000

    Solve Day 2

---
+++ day2/src/main/scala/Main.scala
@@ -0,0 +1,118 @@
+package advent
+
+import scala.collection.SortedMap
+import scala.util.parsing.combinator._
+
+object Day2 extends RegexParsers {
+  // PROBLEM DOMAIN //
+  
+  val ALPHABET: List[Char] = "abcdefghijklmnopqrstuvwxyz".toList
+  
+  /**
+   * Represents a multiset of alphabetical characters.
+   * 
+   * `BoxID`s are elements of the free monoid on 26 generators, while `CharCount`s
+   * are elements of `\mathbb{N}^26`, so we can let the obvious map from the 
+   * 26-fold free product to the 26-fold Cartesian product do the counting.
+   */
+  class CharCount(fromSortedMap: SortedMap[Char,Int]) {
+    val toMap: SortedMap[Char,Int] = fromSortedMap
+    
+    val toList: List[Int] = ALPHABET.map { x => toMap.getOrElse(x,0) }
+    
+    /**
+     * Returns the pointwise sum of two `CharCounts`.
+     */
+    def +(that: CharCount) = new CharCount(
+      SortedMap[Char,Int]() ++ ALPHABET.map { x => x -> (this.toMap(x) + that.toMap(x)) } 
+    )
+     
+    lazy val hasTwoRepeated: Boolean = toMap.values.count(_ == 2) > 0
+    
+    lazy val hasThreeRepeated: Boolean = toMap.values.count(_ == 3) > 0
+    
+    override def equals(that: Any): Boolean = {
+      that.isInstanceOf[CharCount] &&
+      this.toList == that.asInstanceOf[CharCount].toList
+    }
+  }
+  object CharCount {  
+    val empty: CharCount = new CharCount(
+      SortedMap[Char,Int]() ++ ALPHABET.map { x => x -> 0 }
+    )
+    
+    def from(c: Char): CharCount = new CharCount(
+      empty.toMap + (c -> 1)
+    )
+  }
+  
+  
+  case class BoxID(asString: String) {
+    require( asString.forall( c => ALPHABET.contains(c) ) )
+    
+    def charCount: CharCount = 
+      asString.toList.map(CharCount.from).foldLeft(CharCount.empty)(_ + _)
+
+    /**
+     * Finds all possible Hamming distance 1 neighbors of the `BoxID`.
+     */
+    def neighbors: Seq[BoxID] = for {
+      n <- 0 to asString.length
+      c <- ALPHABET
+    } yield BoxID( asString.take(n) + c + asString.drop(n+1) ) 
+
+  }
+  
+  
+  /**
+   * Calculates the checksum of a given list of `BoxID`s, by counting the number that have
+   * an ID containing exactly two of any letter and then separately counting those with
+   * exactly three of any letter, and multiplying th counts together. Solves Part 1.
+   */
+  def checksum(ids: Traversable[BoxID]): Int =
+    ids.count(_.charCount.hasTwoRepeated) * ids.count(_.charCount.hasThreeRepeated)
+    
+  
+  /**
+   * Finds two boxes in the given list which differ by exactly one character at the same
+   * position in both strings, or `None` if no such pair exists. Solves Part 2.
+   * 
+   * Note: this runs in linear time!
+   */
+  def findPrototype(ids: Traversable[BoxID]): Option[(BoxID, BoxID)] = {
+    val idSet: Set[BoxID] = Set() ++ ids
+    val results = for {
+      id <- ids
+      neighbors = id.neighbors.filter(n => idSet.contains(n) && n != id)
+      if (!neighbors.isEmpty)
+    } yield (id,neighbors.head)
+    results.headOption
+  }
+  
+  // INPUT HANDLING //
+  
+  def lineParser: Parser[BoxID] =
+    """[a-z]+""".r ^^ { x => BoxID(x) }
+  
+  def inputParser: Parser[List[BoxID]] = rep(lineParser)
+  
+  
+  // MAIN //
+  def main(args: Array[String]) = {
+    val source = scala.io.Source.fromFile("input.txt")
+    val content = try source.mkString finally source.close
+    
+    parse(inputParser, content) match {
+      case Failure(msg,_)     => println("Parsing failed: " + msg)
+      case Error(msg,_)       => println("Error: " + msg)
+      case Success(matched,_) => {
+        val part1 = checksum(matched)
+        println( "Result of Part 1: " + part1 )
+        val part2 = findPrototype(matched).map(x => x._1.asString intersect x._2.asString)
+        println( "Result of Part 2: " + part2 )
+      }
+    }
+    println("Bye!")
+  }
+  
+}
---
+++ day2/src/test/scala/Test.scala
@@ -0,0 +1,80 @@
+package advent
+
+import org.scalacheck._
+import org.scalacheck.Properties
+import org.scalacheck.Gen._
+import org.scalacheck.Arbitrary._
+
+import scala.util.parsing.combinator._
+import advent.Day2._
+
+object Test extends Properties("Main") {
+  
+  // GENERATORS //
+  val genAlphabetical: Gen[Char] = Gen.oneOf(ALPHABET)
+  
+  val genBoxID: Gen[BoxID] = for {
+    xs <- Gen.listOf( genAlphabetical )
+  } yield BoxID(xs.mkString)
+  implicit val arbBoxID: Arbitrary[BoxID] =
+    Arbitrary(genBoxID)
+  
+  val genCharCount: Gen[CharCount] = for {
+    id <- genBoxID
+  } yield id.charCount
+  implicit val arbCharCount: Arbitrary[CharCount] =
+    Arbitrary(genCharCount)
+  
+  
+  // PROPERTIES //
+  
+  property("CharCount.equals is reflexive") = Prop.forAll { (x: CharCount) =>
+    x == x
+  }
+  
+  property("CharCount.empty is identity element for +") = Prop.forAll { (x: CharCount) =>
+    (x + CharCount.empty) == x
+  }
+  
+  property("CharCount.+ is commutative") = Prop.forAll { (x: CharCount, y: CharCount) =>
+    (x + y) == (y + x)
+  }
+  
+  property("CharCount.+ is associative") = Prop.forAll { (x: CharCount, y: CharCount, z: CharCount) =>
+    (x + y) + z == x + (y + z)
+  }
+  
+  property("BoxID.neighbors contains original element") = Prop.forAll { (x:BoxID) =>
+    x.asString.isEmpty || x.neighbors.contains(x)
+  }
+  
+  property("BoxID.charcount of empty is empty") = Prop {
+    BoxID("").charCount == CharCount.empty
+  }
+  
+  property("BoxID.charcount is monoid homomorphism") = Prop.forAll { (x: BoxID, y: BoxID) =>
+    BoxID(x.asString + y.asString).charCount == x.charCount + y.charCount
+  }
+  
+  property("BoxID.charcount has total equal to length") = Prop.forAll { (x: BoxID) =>
+    x.charCount.toList.sum == x.asString.length
+  }
+  
+  property("BoxID.charcount counts the number of occurrences") = Prop.forAll { (x: BoxID, c: Char) =>
+    x.charCount.toMap.getOrElse(c,0) == x.asString.count(_ == c)
+  }
+  
+  property("checksum is stable under permutations") = Prop.forAll { (xs: List[BoxID], perm: Int) =>
+    val ys = new scala.util.Random(perm).shuffle(xs)
+    checksum(xs) == checksum(ys)
+  }
+  
+  property("checksum of empty list is 0") = Prop {
+    checksum( List() ) == 0
+  }
+  
+  property("checksum is bounded above by length square") = Prop.forAll { (xs: List[BoxID]) =>
+    checksum(xs) <= xs.length*xs.length
+  }
+
+}
